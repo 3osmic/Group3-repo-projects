0a50c2aa688bd5f5e4e9c5fdd891af73e2f57c25
Christopher Barlow <cbarlow6312@gmail.com>
2023-11-21 15:43:35 -0500

    Update

--- registration_testing.py
+++ registration_testing.py
@@ -1,48 +1,58 @@
 import unittest
-from flask import Flask
-from app import app
-import sqlite3
-import init_db
+from app import app, get_db_connection, db
 
-class RegistrationTesting(unittest.TestCase):
 
+class TestRegistration(unittest.TestCase):
     def setUp(self):
-        # Create a test client
-        self.app = app.test_client()
-        self.app.testing = True
+        self.app = app
+        self.app.config['TESTING'] = True
+        self.app.config['SECRET_KEY'] = 'testingdb123'
+        self.app_ctxt = self.app.app_context()
+        self.app_ctxt.push()
+        self.client = self.app.test_client()
+        with get_db_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='users'")
+            table_exists = cursor.fetchone() is not None
 
-        self.conn = sqlite3.connect('database.db')
-        self.conn.row_factory = sqlite3.Row
-        init_db(self.conn)
+        if not table_exists:
+            # Create the users table for testing
+            db(create_table=True)
 
-    def test_validate_signup(self):
-        # Data for the signup form
-        data = dict(
-            username='test_user1',
-            email='new@test.test',
-            password='example',
-            confirm_password='example'
-        )
+    def tearDown(self):
+        with get_db_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute('DROP TABLE IF EXISTS users')
+            conn.commit()
 
-        # Submit the signup form
-        response = self.app.post('/signup', data=data, follow_redirects=False)
+        self.app_ctxt.pop()
 
-        # Follow the redirect manually
-        redirect_response = self.app.get(response.headers['Location'])
+    def test_signup_successful(self):
+        form_data = {
+            'username': b'test_user12345',
+            'email': b'existing@test.test',  # Existing email in the database
+            'password': b'example',
+            'confirm-password': b'example',
+        }
 
-        # Assert that the user is present in the database
-        user_in_database = self.get_user_from_database(data['username'])
-        self.assertIsNotNone(user_in_database)
-        self.assertEqual(user_in_database['username'], data['username'])
+        response = self.client.post('/signup', data=form_data, follow_redirects=True)
 
-    def get_user_from_database(self, username):
-        cursor = self.conn.cursor()
-        cursor.execute('SELECT * FROM users WHERE username = ?', (username,))
-        user = cursor.fetchone()
-        return user
+        print(response.data)
+
+        assert b"Cuisine Page" in response.data
+        assert response.status_code == 200
+        assert response.request.path == '/index'
+
+    def test_validate_password_match(self):
+        response = self.client.post('/signup', data={
+            'username': 'group3',
+            'password': '12345',
+            'confirm_password': '1234'
+        })
+        # assert response.status_code == 200
+        # html = response.get_data(as_text=True)
+        # assert "Field must be equal to password." in html
 
 
 if __name__ == '__main__':
     unittest.main()
-
-
